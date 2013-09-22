cd9c619f0a02e7e2e3e0082ad694a660271a40d7
ldoiron17 <ldoiron17@gmail.com>
2013-09-22 15:48:15 -0700

    First commit

---
+++ Motor_driver/Motor_driver/Motor_driver.c
@@ -0,0 +1,18 @@
+/*
+ * Motor_driver.c
+ *
+ * Created: 9/22/2013 6:37:11 PM
+ *  Author: Cornelius Fudge
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+		//blah blha blahahbhsdbf
+    }
+}
