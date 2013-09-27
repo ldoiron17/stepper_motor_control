f87137f9f596ad35004e51af05a7188c7c650b07
ldoiron17 <ldoiron17@gmail.com>
2013-09-26 19:03:13 -0700

    LED blinkage using ISRs, like a beezleboss

--- Motor_driver/Motor_driver/Motor_driver.c
+++ Motor_driver/Motor_driver/Motor_driver.c
@@ -20,14 +20,32 @@ int main(void)
 {
 	
 	DDRC  = 0b00110000;   // Turn PC4 and PC5 to output (LED1 and LED2)
+	TIMSK1 = _BV(OCIE1A) | _BV(OCIE1B); // Enable Interrupt Timer/Counter1, Output Compare A & B (SIG_OUTPUT_COMPARE1A/SIG_OUTPUT_COMPARE1B)
+	TCCR1B = _BV(CS12) | _BV(CS10) | _BV(WGM12);    // Clock/1024, 0.001024 seconds per tick, Mode=CTC
+    OCR1A = 1500;                       // 0.001024*1954 ~= 2 therefore SIG_OUTPUT_COMPARE1A will be triggered every 2 seconds
+    OCR1B = 977;                       // 0.001024*977 = 1.0004480 therefore SIG_OUTPUT_COMPARE1B will be triggered every second
+	sei();
 
     while(1)
     {
-		_delay_ms(500);
-        led1_on();
-		led2_off();
-		_delay_ms(500);
-		led1_off();
-		led2_on();
+		////_delay_ms(500);
+        ////led1_on();
+		////led2_off();
+		////_delay_ms(500);
+		////led1_off();
+		////led2_on();
     }
 }
+
+ISR(TIMER1_COMPA_vect) //interrupt service routine for timer1 compare A flag
+{
+	led1_on();
+	led2_off();
+}
+
+ISR(TIMER1_COMPB_vect) //interrupt service routine for timer1 compare B flag
+{
+	led1_off();
+	led2_on();
+}
+
